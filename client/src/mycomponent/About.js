1fae09095e8c4ffbb965bb748af6841f77e32b0a
deepaksaini2218 <mkl210016@gmail.com>
2023-07-24 15:17:54 +0530

    Update About.js

--- client/src/mycomponent/About.js
+++ client/src/mycomponent/About.js
@@ -187,7 +187,7 @@ function About(props) {
             <h3 class="resume-title">Professional Experience</h3>
             <div class="resume-item">
               <h4>Intern IT-Executive</h4>
-              <h5>Apr/2023 - Present</h5>
+              <h5>Apr/2023 - Jul/2023</h5>
               <p><em>Ismart TechSpeeds Innovative Electrical Appliances Pvt. Ltd.  </em></p>
               <ul>
                 <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
@@ -199,7 +199,7 @@ function About(props) {
             <h3 class="resume-title">Certifications</h3>
             <div class="resume-item">
               <h4>Basic Computers</h4>
-              <h5>Apr/2023 - Present</h5>
+              <h5> Sep/2017 - Nov/2017</h5>
               <p><em>TINT Computer Center</em></p>
               <ul>
                 <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
@@ -210,7 +210,7 @@ function About(props) {
             </div>
             <div class="resume-item">
               <h4>Full Stack</h4>
-              <h5>Apr/2023 - Present</h5>
+              <h5>Sep/2021 - Feb/2022</h5>
               <p><em>SSDN Technologies </em></p>
               <ul>
                 <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
